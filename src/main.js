d6cf9fb10ecf6716f7beb8cb59dfd4d85fc18414
Eric Fei <guanwha@gmail.com>
2019-08-07 08:30:46 +0800

    Installed vee-validate 2.2.14

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import VeeValidate from 'vee-validate';
 import Vue from 'vue';
 import App from './App';
 import './styles/_reset.css';
 import './styles/_normalize.scss';
 
 Vue.config.productionTip = false;
+Vue.use(VeeValidate);
 
 /* eslint-disable no-new */
 new Vue({
