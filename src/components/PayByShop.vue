a7f3c46deac62d0b4f9b91f960c4bf5e60bf0c50
Eric Fei <guanwha@gmail.com>
2019-08-10 21:59:50 +0800

    Binded data for interaction
    1. add data
       - remove curAreaCode
       - add orderer
       - add recipient
    
    2. copy orderer info to recipient info in switchEqualOrdererInfo()

--- src/components/PayByShop.vue
+++ src/components/PayByShop.vue
@@ -55,7 +55,7 @@
           <div class="col-12 col-lg-1 title">姓名</div>
           <!-- name -->
           <div class="col-12 col-lg-5 field">
-            <input class="form-control" type="text" id="idName" placeholder="請填寫真實姓名">
+            <input class="form-control" type="text" id="idName" v-model="orderer.name" placeholder="請填寫真實姓名">
           </div>
           <div class="col-12 col-lg-1 title">
             <div>手機</div>
@@ -66,19 +66,19 @@
               <button id="idAreaCode"
                       type="button" class="btn btn-dropdown dropdown-toggle"
                       data-toggle="dropdown">
-                {{curAreaCode}}
+                {{orderer.areaCode}}
               </button>
               <div class="dropdown-menu">
                 <div v-for='(country, code) in areaCode'
                     :key='code'>
-                  <a class="dropdown-item" href="#">{{code}} {{country}}</a>
+                  <a class="dropdown-item" href="#" @click="orderer.areaCode = code">{{code}} {{country}}</a>
                 </div>
               </div>
             </div>
           </div>
           <!-- phone code -->
           <div class="col-12 col-lg-4 field">
-            <input class="form-control" type="number" id="idPhone" placeholder="請填寫手機號碼">
+            <input class="form-control" type="number" id="idPhone" v-model="orderer.phone" placeholder="請填寫手機號碼">
           </div>
         </div>
         <!-- orderer address -->
@@ -87,10 +87,10 @@
             <div>地址</div>
           </div>
           <div class="col-12 col-lg-2 field">
-            <input class="form-control" type="text" id="idPostalCode" placeholder="郵遞區號">
+            <input class="form-control" type="text" id="idPostCode" v-model="orderer.postCode" placeholder="郵遞區號">
           </div>
           <div class="col-12 col-lg-9 field">
-            <input class="form-control" type="text" id="idAddress" placeholder="例：新北市信義區復興路999段99號1巷8樓">
+            <input class="form-control" type="text" id="idAddress" v-model="orderer.address" placeholder="例：新北市信義區復興路999段99號1巷8樓">
           </div>
         </div>
         <!-- orderer email -->
@@ -99,7 +99,7 @@
             <div>Email</div>
           </div>
           <div class="col-12 col-lg-11 field">
-            <input class="form-control" type="email" id="idEmail" placeholder="寄送通知會發送至此email">
+            <input class="form-control" type="email" id="idEmail" v-model="orderer.email" placeholder="寄送通知會發送至此email">
           </div>
         </div>
       </div>
@@ -122,7 +122,7 @@
           <div class="col-12 col-lg-1 title">姓名</div>
           <!-- name -->
           <div class="col-12 col-lg-5 field">
-            <input class="form-control" type="text" id="idName" placeholder="請填寫真實姓名">
+            <input class="form-control" type="text" id="idName2" v-model="recipient.name" placeholder="請填寫真實姓名">
           </div>
           <div class="col-12 col-lg-1 title">
             <div>手機</div>
@@ -130,22 +130,22 @@
           <!-- area code -->
           <div class="col-12 col-lg-1 field">
             <div class="dropdown remove-p">
-              <button id="idAreaCode"
+              <button id="idAreaCode2"
                       type="button" class="btn btn-dropdown dropdown-toggle"
                       data-toggle="dropdown">
-                {{curAreaCode}}
+                {{recipient.areaCode}}
               </button>
               <div class="dropdown-menu">
                 <div v-for='(country, code) in areaCode'
                     :key='code'>
-                  <a class="dropdown-item" href="#">{{code}} {{country}}</a>
+                  <a class="dropdown-item" href="#" @click="recipient.areaCode = code">{{code}} {{country}}</a>
                 </div>
               </div>
             </div>
           </div>
           <!-- phone code -->
           <div class="col-12 col-lg-4 field">
-            <input class="form-control" type="number" id="idPhone" placeholder="請填寫手機號碼">
+            <input class="form-control" type="number" id="idPhone2" v-model="recipient.phone" placeholder="請填寫手機號碼">
           </div>
         </div>
         <!-- recipient address -->
@@ -154,10 +154,10 @@
             <div>地址</div>
           </div>
           <div class="col-12 col-lg-2 field">
-            <input class="form-control" type="text" id="idPostalCode" placeholder="郵遞區號">
+            <input class="form-control" type="text" id="idPostCode2" v-model="recipient.postCode" placeholder="郵遞區號">
           </div>
           <div class="col-12 col-lg-9 field">
-            <input class="form-control" type="text" id="idAddress" placeholder="例：新北市信義區復興路999段99號1巷8樓">
+            <input class="form-control" type="text" id="idAddress2" v-model="recipient.address" placeholder="例：新北市信義區復興路999段99號1巷8樓">
           </div>
         </div>
         <!-- recipient email -->
@@ -166,7 +166,7 @@
             <div>Email</div>
           </div>
           <div class="col-12 col-lg-11 field">
-            <input class="form-control" type="email" id="idEmail" placeholder="寄送通知會發送至此email">
+            <input class="form-control" type="email" id="idEmail2" v-model="recipient.email" placeholder="寄送通知會發送至此email">
           </div>
         </div>
       </div>
@@ -220,12 +220,27 @@ export default {
   data() {
     return {
       titleIcon: './static/images/icon_shop.svg',
-      curAreaCode: 886,
       areaCode: {
         886: '台灣',
         86: '中國',
         81: '日本',
         82: '韓國',
+      },
+      orderer: {
+        name: '',
+        areaCode: '886',
+        phone: '',
+        postCode: '',
+        address: '',
+        email: '',
+      },
+      recipient: {
+        name: '',
+        areaCode: '886',
+        phone: '',
+        postCode: '',
+        address: '',
+        email: '',
       },
       isSameOrdererInfo: false,
       remarkChecked: false,
@@ -234,6 +249,9 @@ export default {
   methods: {
     switchEqualOrdererInfo() {
       this.isSameOrdererInfo = !this.isSameOrdererInfo;
+      if (this.isSameOrdererInfo) {
+        this.recipient = Object.assign({}, this.orderer);   // copy
+      }
     },
     switchRemarkChecked() {
       this.remarkChecked = !this.remarkChecked;
